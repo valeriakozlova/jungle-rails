4143ddc0593c287f9f5459694767c876771e4ef5
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-12-06 23:28:43 +0000

    Added admin authentication

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,8 @@
 class Admin::CategoriesController < ApplicationController
+  
+  http_basic_authenticate_with name: ENV["AUTHENTICATE_NAME"], password: ENV["AUTHENTICATE_PASSWORD"]
+
+
   def index
     @categories = Category.all
   end
--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["AUTHENTICATE_NAME"], password: ENV["AUTHENTICATE_PASSWORD"]
+
+
   def show
   end
 end
